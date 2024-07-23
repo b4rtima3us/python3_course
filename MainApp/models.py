540094f5a0b32c823b9d74b5a56c5b5d4e841871
G. Volk-Karachevskiy <bartima3us@yandex.ru>
2024-07-23 16:59:44 +0300

    add default description

--- MainApp/models.py
+++ MainApp/models.py
@@ -6,4 +6,4 @@ class Item(models.Model):
     name = models.CharField(max_length=100)
     brand = models.CharField(max_length=100)
     count = models.PositiveIntegerField()
-    description = models.CharField(max_length=150, null=True)
+    description = models.CharField(max_length=150, default=None)
